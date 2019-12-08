7be5ee3956a9fef5bc3cf7a19d14f54348f15511
sumedhvaidy <coolsumedhv@gmail.com>
2019-12-08 17:39:10 -0600

    5

--- app/src/main/java/com/example/moodtunes/Connectors/UserService.java
+++ app/src/main/java/com/example/moodtunes/Connectors/UserService.java
@@ -45,40 +45,4 @@ public class UserService {
         };
         mqueue.add(jsonObjectRequest);
     }
-
-    /*
-    private static final String ENDPOINT = "https://api.spotify.com/v1/me";
-    private SharedPreferences msharedPreferences;
-    private RequestQueue mqueue;
-    private User user;
-
-    public UserService(RequestQueue queue, SharedPreferences sharedPreferences) {
-        mqueue = queue;
-        msharedPreferences = sharedPreferences;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void get(final VolleyCallBack callBack) {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(ENDPOINT, null, response -> {
-            Gson gson = new Gson();
-            user = gson.fromJson(response.toString(), User.class);
-            callBack.onSuccess();
-        }, error -> get(() -> {
-
-        })) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> headers = new HashMap<>();
-                String token = msharedPreferences.getString("token", "");
-                String auth = "Bearer " + token;
-                headers.put("Authorization", auth);
-                return headers;
-            }
-        };
-        mqueue.add(jsonObjectRequest);
-    }
-     */
 }
--- app/src/main/java/com/example/moodtunes/PlaylistScreen.java
+++ app/src/main/java/com/example/moodtunes/PlaylistScreen.java
@@ -5,35 +5,83 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-import org.w3c.dom.Text;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlaylistScreen extends AppCompatActivity {
+
+    public List<Song> songs = new ArrayList<Song>();
+    public Song song = new Song();
+    public String pickedMood;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist_screen);
         Intent moodIntent = getIntent();
-        String pickedMood = moodIntent.getStringExtra("mood");
-        System.out.println("after: " + pickedMood);
+        pickedMood = moodIntent.getStringExtra("mood");
+        Log.i("blah", pickedMood);
         TextView mood = findViewById(R.id.mood);
         mood.bringToFront();
         mood.setTextColor(Color.parseColor("#FFFFFF"));
         mood.setText(pickedMood);
         Button backButton = findViewById(R.id.backButton);
         backButton.bringToFront();
+
+        readCSV();
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+
     }
+
+    public void readCSV() {
+        InputStream csvStream = getResources().openRawResource(R.raw.tagmoods);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(csvStream, Charset.forName("UTF-8")));
+        String songRawString = "";
+
+        try {
+            while((songRawString = reader.readLine()) != null) {
+                String[] fields = songRawString.split(",");
+                if (fields.length > 0) {
+                    if (fields[3].equals(pickedMood)) {
+                        song.setNumber(fields[0]);
+                        song.setArtist(fields[1]);
+                        song.setTitle(fields[2]);
+                        song.setMood(fields[3]);
+                        songs.add(song);
+                        Log.i("BLAH","Songs added a song");
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            // do some logging
+        }
+        finally {
+            try {
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        //System.out.println("BLAH" + songs.size());
+
+    }
+
 }
